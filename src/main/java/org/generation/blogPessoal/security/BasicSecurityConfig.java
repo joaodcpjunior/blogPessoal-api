763645ed7a51e6f45039cece367dfa63c4ee7060
João Junior <joaodcpjunior@gmail.com>
2021-08-11 11:48:42 -0300

    Adicionado usuario admin e liberado acesso de todos os endpoints

--- src/main/java/org/generation/blogPessoal/security/BasicSecurityConfig.java
+++ src/main/java/org/generation/blogPessoal/security/BasicSecurityConfig.java
@@ -2,6 +2,7 @@ package org.generation.blogPessoal.security;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -12,14 +13,17 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
 @EnableWebSecurity
-public class BasicSecurityConfig extends WebSecurityConfigurerAdapter{
-    
+public class BasicSecurityConfig extends WebSecurityConfigurerAdapter {
+
     @Autowired
-    private UserDetailsService userDetailsService;
+    private UserDetailsService service;
 
     @Override
-    protected void configure(AuthenticationManagerBuilder auth) throws Exception{
-        auth.userDetailsService(userDetailsService);
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.inMemoryAuthentication().withUser("admin").password(passwordEncoder().encode("admin"))
+                .authorities("ROLE_ADMIN");
+
+        auth.userDetailsService(service);
     }
 
     @Bean
@@ -28,15 +32,11 @@ public class BasicSecurityConfig extends WebSecurityConfigurerAdapter{
     }
 
     @Override
-    protected void configure(HttpSecurity http) throws Exception{
-        http.authorizeRequests()
-        .antMatchers("/usuarios/logar").permitAll()
-        .antMatchers("/usuarios/cadastrar").permitAll()
-        .anyRequest().authenticated()
-        .and().httpBasic()
-        .and().sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
-        .and().cors()
-        .and().csrf().disable();
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests().antMatchers("/**").permitAll().antMatchers(HttpMethod.POST, "/api/v1/usuario/salvar")
+                .permitAll().antMatchers(HttpMethod.PUT, "/api/v1/usuario/autenticar").permitAll().anyRequest()
+                .authenticated().and().httpBasic().and().sessionManagement()
+                .sessionCreationPolicy(SessionCreationPolicy.STATELESS).and().cors().and().csrf().disable();
     }
 
 }
